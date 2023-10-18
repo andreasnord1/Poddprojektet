494f294cd67c3576a66a838f1a0f8d35870ad8cd
vickrv221 <105204504+vickrv221@users.noreply.github.com>
2023-10-18 17:50:44 +0200

    deserialiserare
    This reverts commit 952027547ee9fabe816a0f547b83eed653e9696b.

--- DataAccessLayer/Serialiserare.cs
+++ DataAccessLayer/Serialiserare.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; // Tillagd för att kunna använda FileStream.
 using System.Xml.Serialization;
 using Models;
 
@@ -10,23 +11,49 @@ namespace DataAccessLayer
 {
     internal class Serialiserare
     {
+        private static readonly string filePath = "Podcasts.xml"; 
+        // Centraliserar filnamnet så att man enkelt kan ändra det senare om det behövs.
+
         public void SerialiseraPodcasts(List<Podcast> podcastLista)
         {
             try
             {
                 XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Podcast>));
-                using (FileStream sparningsFil = new FileStream("Persons.xml", FileMode.Create, FileAccess.Write))
+                using (FileStream sparningsFil = new FileStream(filePath, FileMode.Create, FileAccess.Write))
                 {
                     xmlSerializer.Serialize(sparningsFil, podcastLista);
                 }
             }
             catch (Exception ex)
             {
-                //Här ska något in senare!! 
+                // Visar ett felmeddelande för användaren, eller kastar om det till en högre nivå.
+                throw new Exception("Ett fel uppstod vid serialisering av podcasts.", ex);
             }
         }
 
-        // Här ska en DeSerialiserar-metod skapas!! 
+        
+        public List<Podcast> DeSerialisera()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Podcast>));
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    return (List<Podcast>)xmlSerializer.Deserialize(fileStream);
+                }
+            }
+            catch (FileNotFoundException) // Första gången programmet körs kanske filen inte finns, så vi returnerar en ny lista.
+            {
+                return new List<Podcast>();
+            }
+            catch (Exception ex)
+            {
+                // Ett oväntat fel inträffade, lägger till mer kontext och kastar om undantaget för ytterligare hantering uppåt.
+                throw new Exception("Ett fel uppstod vid deserialisering av podcasts.", ex);
+               
+            }
+        }
+
 
 
 
